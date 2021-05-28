bcaf9d49d301997629af6ea739c74d1171980344
Carlos Guzman <cags84@gmail.com>
2021-05-27 19:29:59 -0500

    Add react a basic example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import "./styles.css";
+import React from "react";
+import ReactDom from "react-dom";
 
-document.getElementById("app").innerHTML = ``;
+const root = document.getElementById("app");
+
+const element = (
+  <div className="container">
+    <p>Hello world</p>
+  </div>
+);
+
+ReactDom.render(element, root);
+
+/*
+const element = React.createElement(
+  "div",
+  {
+    className: "container"
+  },
+  "Hola mundo",
+  "Hoy es Jueves"
+);
+
+ReactDom.render(element, root);
+*/
+/*
+
+const root = document.getElementById("app");
+
+const element = document.createElement("div");
+element.className = "container";
+element.textContent = "Hello world";
+
+root.appendChild(element);
+*/
